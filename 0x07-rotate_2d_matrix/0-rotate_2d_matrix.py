aa233b6343ae87bb672b31788befae05fa348ba8
Alatise Oluwaseun <holynation667@gmail.com>
2023-03-23 16:17:46 +0100

    feat: Added code to rotate 2-D

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module contains a function that rotates a 2D matrix by 90 degrees
+in a clockwise direction
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotate a 2d square matrix 90 degrees clockwise in-place
+    Args:
+        matrix (list): 2d square matrix
+    Return:
+        None
+    """
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
+
+    for i in range(n):
+        for j in range(int(n / 2)):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[i][n-1-j]
+            matrix[i][n-1-j] = temp
