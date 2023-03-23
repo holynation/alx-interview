6c58f0772bade46922bc808b7368b6cead9b39dc
Alatise Oluwaseun <holynation667@gmail.com>
2023-03-23 15:14:44 +0100

    fix: Adjusted the code

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Performs log parsing from stdin"""
-
-import re
+"""
+module contains a script that reads stdin line by line and computes metrics
+"""
 import sys
-counter = 0
-file_size = 0
-status_code_counter = {
+
+
+status_codes = {
     "200": 0,
     "301": 0,
     "400": 0,
@@ -16,33 +16,33 @@ status_code_counter = {
     "500": 0
 }
 
+file_size = 0
 
-def print_metrics(dict, file_s):
-    """Prints the status code and the number of times they appear"""
-    print("File size: {}".format(file_s))
-    for key in sorted(dict.keys()):
-        if status_code_counter[key] != 0:
-            print("{}: {}".format(key, dict[key]))
+
+def print_metrics():
+    """prints of the logs"""
+    print("File size: {}".format(file_size))
+    for status in sorted(status_codes.keys()):
+        if status_codes[status]:
+            print("{}: {}".format(status, status_codes[status]))
 
 
 if __name__ == "__main__":
+    count = 0
     try:
         for line in sys.stdin:
-            split_strings = re.split('- |"|"| " " ', str(line))
-            statusC_and_file_s = split_strings[-1]
-            if counter != 0 and counter % 10 == 0:
-                print_metrics(status_code_counter, file_size)
-            counter = counter + 1
             try:
-                statusC = int(statusC_and_file_s.split()[0])
-                f_size = int(statusC_and_file_s.split()[1])
-                # print("Status Code {} size {}".format(statusC, f_size))
-                if statusC in status_code_counter:
-                    status_code_counter[statusC] += 1
-                file_size = file_size + f_size
-            except:
+                elems = line.split()
+                file_size += int(elems[-1])
+                if elems[-2] in status_codes:
+                    status_codes[elems[-2]] += 1
+            except Exception:
                 pass
-        print_metrics(status_code_counter, file_size)
+            if count == 9:
+                print_metrics()
+                count = -1
+            count += 1
     except KeyboardInterrupt:
-        print_metrics(status_code_counter, file_size)
+        print_metrics()
         raise
+    print_metrics()
